43b996274b24306ee7d6b865804a7a26859772f9
Pieter <pieter.bergwerff@sdbgroep.nl>
2026-01-04 11:44:00 +0100

    feat: create Admin template component and refactor template exports

--- apps/app/src/app/template.tsx
+++ apps/app/src/app/template.tsx
@@ -1,11 +1,1 @@
-// import components
-import Template from '@packages/components/templates/Default'
-
-// import types
-import type { FC, PropsWithChildren } from 'react'
-
-export const AppTemplate: FC<PropsWithChildren> = ({ children }) => {
-  return <Template>{children}</Template>
-}
-
-export default AppTemplate
+export { default } from '@packages/components/templates/Default'
